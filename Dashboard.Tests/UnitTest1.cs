a539e83cd4ff7fce3b9b746ce1f947b4f533ddec
jacob-enon <66114041+jacob-enon@users.noreply.github.com>
2022-10-06 12:22:03 +0100

    Removed python app, added C# project (#9)
    * Deleted python app
    
    * Delete python app
    
    * Added ECM3440 solution
    
    * reset to main
    
    * Commented out breaking code
    
    * Deleted python
    
    * Rider gitignore
    
    * Fixed Program.cs and deleted .idea
    
    * deleted ECM3440 folder
    
    * removed workflow
    
    * added dotnet workflow
    
    Co-authored-by: exe-je437 <je437@exeter.ac.uk>

---
+++ Dashboard.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Dashboard.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
---
+++ SoilMoistureSensor/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Devices.Client;
+
+Console.WriteLine();
+
+// var conn = Microsoft.Azure.Devices.Client.IotHubConnectionStringBuilder.Create("");
+// var client = Microsoft.Azure.Devices.Client.DeviceClient.Create(conn);
+
+// IOT connection
+// Don't need CounterFit - can just send any data
+
+
+// Connection to IOT
+// Create client for IOT & connect
+
+// Test project for soil moisture sensor
+// Test project for dashboard
+// Dashboard project
