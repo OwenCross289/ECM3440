5708468c6e13c60c7c2aeb04126193e7123c725c
jacob-enon <66114041+jacob-enon@users.noreply.github.com>
2022-11-10 14:16:36 +0000

    Ecm 21 charting apex charts (#50)
    * POC Apex Chart with AddPoint method
    
    * Added datetime to output
    
    * Better AddPlot implementation whilst I fix IoT hub event
    
    * Chart updates live on soil moisture push
    
    * gitignore coverage reports
    
    Co-authored-by: Jacob Enon <je150221@renishaw.com>
    Co-authored-by: exe-je437 <je437@exeter.ac.uk>

--- Dashboard/Models/SoilMoisture.cs
+++ Dashboard/Models/SoilMoisture.cs
@@ -1,7 +1,7 @@
-namespace Dashboard.Models
+namespace Dashboard.Models;
+
+public class SoilMoisture
 {
-    public class SoilMoisture
-    {
-        public double Value { get; init; }
-    }
+   public DateTime Time { get; init; }
+   public double Value { get; init; }
 }
