6f2f910589ded054e64475a13c0973d4f743a334
exe-je437 <je437@exeter.ac.uk>
2022-11-10 13:10:53 +0000

    added more coverage

--- Dashboard.Tests/UnitTest1.cs
+++ Dashboard.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using Dashboard;
+using Dashboard.Models;
+
 namespace Dashboard.Tests;
 
 public class UnitTest1
@@ -13,5 +15,16 @@ public class UnitTest1
     public void CheckConstant()
     {
         Assert.Equal("ecm3440-telemetry-queue", Constants.QueueName);
+    }
+
+    [Fact]
+    public void CheckSoilMoistureConstruction()
+    {
+        var sut = new SoilMoisture
+        {
+            Value = 5
+        };
+        
+        Assert.Equal(5, sut.Value);
     }
 }
