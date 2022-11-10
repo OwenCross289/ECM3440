7f6e528f597c8ae0cce2d47190985f04948d5dbd
jacob-enon <66114041+jacob-enon@users.noreply.github.com>
2022-11-10 14:54:32 +0000

    Productionise v0.1-Charting (#51)
    * comic sans (#46)
    
    Co-authored-by: exe-je437 <je437@exeter.ac.uk>
    
    * remove kube.yml (#49)
    
    Co-authored-by: Owen Cross <Owencross99@gmail.com>
    
    * Ecm 39 Code Coverage (#40)
    
    Added coverage to the pipeline
    
    Co-authored-by: exe-je437 <je437@exeter.ac.uk>
    
    * Ecm 21 charting apex charts (#50)
    
    * POC Apex Chart with AddPoint method
    
    * Added datetime to output
    
    * Better AddPlot implementation whilst I fix IoT hub event
    
    * Chart updates live on soil moisture push
    
    * gitignore coverage reports
    
    Co-authored-by: Jacob Enon <je150221@renishaw.com>
    Co-authored-by: exe-je437 <je437@exeter.ac.uk>
    
    Co-authored-by: exe-je437 <je437@exeter.ac.uk>
    Co-authored-by: Owen Cross <59806069+OwenCross289@users.noreply.github.com>
    Co-authored-by: Owen Cross <Owencross99@gmail.com>
    Co-authored-by: Jacob Enon <je150221@renishaw.com>

--- Dashboard.Tests/UnitTest1.cs
+++ Dashboard.Tests/UnitTest1.cs
@@ -1,3 +1,6 @@
+using Dashboard;
+using Dashboard.Models;
+
 namespace Dashboard.Tests;
 
 public class UnitTest1
@@ -6,5 +9,22 @@ public class UnitTest1
     public void Test1()
     {
         Assert.True(true);
+    }
+
+    [Fact]
+    public void CheckConstant()
+    {
+        Assert.Equal("ecm3440-telemetry-queue", Constants.QueueName);
+    }
+
+    [Fact]
+    public void CheckSoilMoistureConstruction()
+    {
+        var sut = new SoilMoisture
+        {
+            Value = 5
+        };
+        
+        Assert.Equal(5, sut.Value);
     }
 }
