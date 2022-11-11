c542e36381d6b29b0f8d193bb6086f74b3f941c5
exe-je437 <je437@exeter.ac.uk>
2022-11-11 07:51:18 +0000

    Updated coverage settings for SoilMoistureSensor

--- SoilMoistureSensor/Program.cs
+++ SoilMoistureSensor/Program.cs
@@ -1,14 +1,22 @@
-﻿using SoilMoistureSensor;
+﻿using System.Diagnostics.CodeAnalysis;
+using SoilMoistureSensor;
 
-Console.WriteLine("Connecting");
-var IoTHubService = new IoTHubService("HostName=ECM3440.azure-devices.net;DeviceId=soil-moisture-sensor;SharedAccessKey=VGlo7rYmMZJVcVaJIehiOgF0QnUCmh3FHFxfpu9RukY=");
-Console.WriteLine("Connected");
-var rand = new Random();
-
-while (true)
+[ExcludeFromCodeCoverage]
+class Program
 {
-    var message = new SoilMoistureStatus { Value = rand.NextDouble() * 1023 };
-    await IoTHubService.SendAsync(message);
-    Console.WriteLine($"Soil Moisture: {message.Value}");
-    Thread.Sleep(10000);
+    public async static Task Main(string[] args)
+    {
+        Console.WriteLine("Connecting");
+        var IoTHubService = new IoTHubService("HostName=ECM3440.azure-devices.net;DeviceId=soil-moisture-sensor;SharedAccessKey=VGlo7rYmMZJVcVaJIehiOgF0QnUCmh3FHFxfpu9RukY=");
+        Console.WriteLine("Connected");
+        var rand = new Random();
+
+        while (true)
+        {
+            var message = new SoilMoistureStatus { Value = rand.NextDouble() * 1023 };
+            await IoTHubService.SendAsync(message);
+            Console.WriteLine($"Soil Moisture: {message.Value}");
+            Thread.Sleep(10000);
+        }
+    }
 }
